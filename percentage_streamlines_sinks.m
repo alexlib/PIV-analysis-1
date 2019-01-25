1576f60f9c035b03c60b1f873e0167a10b325459
Unknown <laste.marcotti@gmail.com>
2019-01-25 11:28:15 +0000

    update percentage_streamlines_sinks.m
    rename/update variables for consistency, delete unused variables

--- percentage_streamlines_sinks.m
+++ percentage_streamlines_sinks.m
@@ -27,17 +27,46 @@ for k = 1:nt
     stream_f = stream(k).f(:);
     [stream_f_sorted, stream_f_sorted_index] = sort(stream_f, 'descend');
     
-    % highest frequency of streamlines at an end point
-    f1 = stream_f_sorted(1,1);
-    f2 = stream_f_sorted(2,1);
-    f3 = stream_f_sorted(3,1);
+    if length(stream_f) >= 3
+        
+        % highest frequency of streamlines at an end point
+        f1 = stream_f_sorted(1,1);
+        f2 = stream_f_sorted(2,1);
+        f3 = stream_f_sorted(3,1);
+        
+        % sum of all streamlines at all end points
+        f_sum = sum(stream(k).f(:));
+        
+        f_percent(k,1) = f1/f_sum * 100;
+        f_percent(k,2) = f2/f_sum * 100;
+        f_percent(k,3) = f3/f_sum * 100;
+        
+    elseif length(stream_f) == 2
+        
+        % highest frequency of streamlines at an end point
+        f1 = stream_f_sorted(1,1);
+        f2 = stream_f_sorted(2,1);
+        
+        % sum of all streamlines at all end points
+        f_sum = sum(stream(k).f(:));
+        
+        f_percent(k,1) = f1/f_sum * 100;
+        f_percent(k,2) = f2/f_sum * 100;
+        
+    elseif length(stream_f) == 1
+        
+        % highest frequency of streamlines at an end point
+        f1 = stream_f_sorted(1,1);
+        
+        % sum of all streamlines at all end points
+        f_sum = sum(stream(k).f(:));
+        
+        f_percent(k,1) = f1/f_sum * 100;
+       
+    end
     
-    % sum of all streamlines at all end points
-    f_sum = sum(stream(k).f(:));
-    
-    f_percent(k,1) = f1/f_sum * 100;
-    f_percent(k,2) = f2/f_sum * 100;
-    f_percent(k,3) = f3/f_sum * 100;
+    clear f1 f2 f3
+    clear stream_f
     
 end
 
